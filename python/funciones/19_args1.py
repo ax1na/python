e4e1f1b210af4ff19468e28a3c5986e9f31d9b55
ax1na <jade.rojas.navarro@alumnos.sip.cl>
2025-04-28 12:29:45 -0400

    Agregar archivos 18_arg.py, 19_args1.py y 20_kwargs.py con funciones de suma

---
+++ python/funciones/19_args1.py
@@ -0,0 +1,9 @@
+#jade rojas}}
+#28-4-25
+def suma(*args):
+   s=0
+   for arg in args:
+       s+=arg
+   return s
+resultado = suma(1,2,3,4,)
+print(resultado) 
---
+++ python/funciones/20_kwargs.py
@@ -0,0 +1,5 @@
+#jade ropjas
+#28-4-25
+def suma(**kwargs):
+    print(type(kwargs))
+suma (y=4, x=8, a=6, C=1) 
